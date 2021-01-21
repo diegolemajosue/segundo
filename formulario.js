1b2d38b41e8ea8ceed8027a4c1fba5116fbbd265
Diego lema <diegolemajosue2004@gmail.com>
2021-01-21 10:28:37 -0500

    21/01/2021 Primera version del programa

---
+++ formulario.js
@@ -0,0 +1,134 @@
+$(document).on("click","#btn-registrar",function(){
+  Swal.fire({
+    icon:"siccess"
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*<script type="text/javascript">
+
+
+   ///srring o cadenas de caracteres; numeros enteros integer
+  ///numeros reales o decimales; float
+  ///fecha - hora, byte
+ /// alert('al inicio de la pagina')
+   //let in=document.getElementBiId('last-name');
+//console.log(In.value);
+ window.addEventListener("load",()=> {
+ document.getElementById("btn-registrar").addEventListener(
+  "click",()=>{
+window.addEventListener("load",()=>{
+  document.getElementById("btn_registrar").addEventListener("click",()=>{
+    let ap=document.getElementById("last_name")
+    let nm=document.getElementById("name")
+    let user=document.getElementById("user")
+    let password=document.getElementById("password")
+    let pais=document.getElementById("pais")
+    alert("Bienvenid@: "+ap.value+nm.value)
+
+  if(ap.value.trim().length<6){
+  Swal.fire({
+  position: 'top-end',
+  icon: 'error',
+  title: 'el apellido es obligatorio',
+  showConfirmButton: true,
+  timer: 5000
+})
+  return false;
+}
+  if(nm.value.trim().length<6){
+  Swal.fire({
+  position: 'top-end',
+  icon: 'error',
+  title: 'el nombre es obligatorio',
+  showConfirmButton: true,
+  timer: 5000
+})
+  return false;
+      }
+  if(user.value.trim().length<6){
+  Swal.fire({
+  position: 'top-end',
+  icon: 'error',
+  title: 'el usuario es obligatorio',
+  showConfirmButton: true,
+  timer: 5000
+})
+  return false;
+}
+  if(password.value.trim().length<6){
+  Swal.fire({
+  position: 'top-end',
+  icon: 'error',
+  title: 'la contraseña es obligatorio',
+  showConfirmButton: true,
+  timer: 5000
+})
+  return false;
+}
+  if(pais.value.trim().length<6){
+  Swal.fire({
+  position: 'top-end',
+  icon: 'error',
+  title: 'el pais es obligatoria',
+  showConfirmButton: true,
+  timer: 5000
+})
+  return false;
+}
+
+    })
+  })
+*/
